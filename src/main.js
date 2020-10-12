fdaf65afa3186edafc9b13dfe1d16af208bd2060
Sören Oehding <soeren.oehding@mondia.com>
2020-10-12 13:30:10 +0200

    [UPDATE][Vue3] updated to Vue v3.0

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue, { createApp } from 'vue';
 import App from './App.vue'
 import Buefy from 'buefy'
 import VueSimpleSVG from 'vue-simple-svg'
@@ -10,8 +10,4 @@ Vue.use(VueMapbox, { mapboxgl: Mapbox })
 Vue.use(VueSimpleSVG)
 Vue.use(Buefy)
 
-Vue.config.productionTip = false
-
-new Vue({
-    render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
