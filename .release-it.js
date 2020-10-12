8dcc57958472d1a5a080c8a476af68e4a895b30e
Sören Oehding <soeren.oehding@mondia.com>
2020-10-12 13:42:31 +0200

    [ReleaseManagement] added release-it and auto-changelog

---
+++ .release-it.js
@@ -0,0 +1,31 @@
+module.exports = {
+  git: {
+    changelog: "npx auto-changelog --stdout -p --commit-limit false --unreleased --template https://raw.githubusercontent.com/release-it/release-it/master/templates/changelog-compact.hbs",
+    requireCleanWorkingDir: true,
+    requireBranch: false,
+    requireUpstream: true,
+    requireCommits: false,
+    addUntrackedFiles: false,
+    commit: true,
+    commitMessage: 'Release ${version}',
+    commitArgs: [],
+    tag: true,
+    tagName: 'v${version}',
+    tagAnnotation: 'Release ${version}',
+    tagArgs: [],
+    push: true,
+    pushRepo: repoUrl,
+    pushArgs: [
+      '--follow-tags'
+    ]
+  },
+  npm: {
+    publish: false,
+    ignoreVersion: true,
+    skipChecks: true
+  },
+  plugins: {},
+  hooks: {
+    'after:bump': "npx auto-changelog -p --commit-limit false https://raw.githubusercontent.com/release-it/release-it/master/templates/keepachangelog.hbs"
+  }
+}
